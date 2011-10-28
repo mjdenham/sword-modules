ab8de949443f77e777d03d1f4b3a30c2b9d6fe89
Martin Denham <mjdenham@gmail.com>
2011-10-28 20:34:38 +0100

    resync

--- JESermons/src/org/andbible/modules/creation/TidyCharacters.java
+++ JESermons/src/org/andbible/modules/creation/TidyCharacters.java
@@ -9,8 +9,9 @@ public class TidyCharacters {
 
 	String filter(String in) {
 		// there a lot of double hyphens that confuse TTS but don't remove them from xml comments
-		return in.replaceAll("([^!])--([^>])", "$1-$2")
+		return 	in.replaceAll("'em", "them") 
 				 .replaceAll("&c;", "etc.")
 				 .replaceAll("&c", "etc.");
+		//in.replaceAll("([^!])--([^>])", "$1-$2")
 	}
 }

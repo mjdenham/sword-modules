27b3e0974af9e4068f07692723f3ed0719fddf3f
Martin Denham <mjdenham@gmail.com>
2011-11-29 19:41:03 +0000

    final fixes to JCRHoliness

--- SwordModuleCreation/src/org/andbible/modules/creation/OSISHelper.java
+++ SwordModuleCreation/src/org/andbible/modules/creation/OSISHelper.java
@@ -7,6 +7,7 @@ public class OSISHelper {
 		return text .replace(",", "")
 					.replace(" - ", " ")
 					.replace("'", "")
+					.replace("?", "")
 					.replace("\"", "")
 					.replaceAll("<reference.*>(.*)</reference>", "$1");
 
